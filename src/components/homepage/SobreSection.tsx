4f62b81354bec6b6a821e8aff6974723e7922909
Patrícia Silva <psilva@imaginarycloud.com>
2026-01-25 03:10:32 +0000

    primeira versão do about me

--- src/components/homepage/SobreSection.tsx
+++ src/components/homepage/SobreSection.tsx
@@ -1,24 +1,45 @@
 import { Typography } from "@mui/material";
+import patsilvarteAboutmePhoto from "../../assets/Patsilvart_me_photo.png";
+import "./SobreSection.scss";
 
 export const SobreSection = () => {
   return (
-    <div>
-      <Typography variant="h3" gutterBottom>
-        Sobre
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        O Patsilvarte Weddings nasceu da vontade de transformar histórias reais
-        em detalhes com significado.
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Depois de criar toda a papelaria e decoração do meu próprio casamento,
-        percebi que queria ajudar outros casais a viver o mesmo — um processo
-        cuidado, próximo e pensado ao pormenor.
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Cada projeto é desenvolvido com tempo, intenção e uma atenção especial
-        aos detalhes que fazem a diferença
-      </Typography>
+    <div className="about-section">
+      <div className="about-section__text">
+        <Typography variant="h3" gutterBottom>
+          Por detrás do Patsilvarte Weddings
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Olá! O meu nome é Patrícia Silva (agora também Fernandes), casei-me em
+          2024 e agora ajudo outros noivos a concretizar o seu sonho através de
+          serviços personalizados de papelaria e decoração.
+        </Typography>
+        <br />
+        <Typography variant="body1" gutterBottom>
+          Sou amante de trabalhos manuais desde que me lembro, de séries
+          românticas que nos deixam a "chorar baba e ranho" e de adoro ajudar a
+          resolver problemas.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Depois de criar toda a papelaria e decoração do meu próprio casamento,
+          percebi que queria ajudar outros casais a viver o mesmo — um processo
+          cuidado, próximo e pensado ao pormenor.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Assim nasceu o Patsilvarte Weddings, da vontade de transformar
+          histórias reais em detalhes com significado.
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Aqui cada projeto é desenvolvido com tempo, intenção e uma atenção
+          especial aos detalhes que fazem a diferença, para que tenhas o dia dos
+          teus sonhos.
+        </Typography>
+      </div>
+      <img
+        alt="Patsilvarte Weddings founder Patricia Silva Fernandes"
+        className="about-section__image"
+        src={patsilvarteAboutmePhoto}
+      />
     </div>
   );
 };
