a3e56345817093b0b83d3d922503f6e67695e167
Patrícia Silva <psilva@imaginarycloud.com>
2026-01-26 03:32:48 +0000

    prepare code for .heic files

---
+++ src/custom.d.ts
@@ -0,0 +1,4 @@
+declare module "*.HEIC" {
+  const value: string;
+  export default value;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   base: "/patsilvarte-weddings/", // repo name
+  assetsInclude: ["**/*.HEIC"],
 });
