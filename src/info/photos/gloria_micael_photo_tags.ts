337da4ae85cd7f00d31efab8a906e2deb1c1f455
Patrícia Silva <psilva@imaginarycloud.com>
2026-01-26 03:33:06 +0000

    add gloria e micael photos

---
+++ src/info/photos/gloria_micael_photo_tags.ts
@@ -0,0 +1,172 @@
+import IMG_5073 from "../../assets/gloria_micael/IMG_5073.HEIC";
+import IMG_5081 from "../../assets/gloria_micael/IMG_5081.HEIC";
+import IMG_8462 from "../../assets/gloria_micael/IMG_8462.HEIC";
+import IMG_8736 from "../../assets/gloria_micael/IMG_8736.HEIC";
+import IMG_8747 from "../../assets/gloria_micael/IMG_8747.HEIC";
+import IMG_8762 from "../../assets/gloria_micael/IMG_8762.HEIC";
+import IMG_8828 from "../../assets/gloria_micael/IMG_8828.HEIC";
+import IMG_8830 from "../../assets/gloria_micael/IMG_8830.HEIC";
+import IMG_8838 from "../../assets/gloria_micael/IMG_8838.HEIC";
+import IMG_8841 from "../../assets/gloria_micael/IMG_8841.HEIC";
+import IMG_8847 from "../../assets/gloria_micael/IMG_8847.HEIC";
+import IMG_8851 from "../../assets/gloria_micael/IMG_8851.HEIC";
+import IMG_8853 from "../../assets/gloria_micael/IMG_8853.HEIC";
+import IMG_8854 from "../../assets/gloria_micael/IMG_8854.HEIC";
+import IMG_8861 from "../../assets/gloria_micael/IMG_8861.HEIC";
+import IMG_8867_1 from "../../assets/gloria_micael/IMG_8867-1.HEIC";
+import IMG_8877 from "../../assets/gloria_micael/IMG_8877.HEIC";
+import IMG_8926 from "../../assets/gloria_micael/IMG_8926.HEIC";
+import IMG_8937 from "../../assets/gloria_micael/IMG_8937.HEIC";
+import IMG_8957 from "../../assets/gloria_micael/IMG_8957.HEIC";
+import IMG_9253 from "../../assets/gloria_micael/IMG_9253.HEIC";
+import IMG_9255 from "../../assets/gloria_micael/IMG_9255.HEIC";
+import IMG_9272 from "../../assets/gloria_micael/IMG_9272.HEIC";
+import IMG_9275 from "../../assets/gloria_micael/IMG_9275.HEIC";
+import { type PhotoInfo, WeddingServices } from "../types";
+
+export const gm_IMG_5073_photo: PhotoInfo = {
+  src: IMG_5073,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_5081_photo: PhotoInfo = {
+  src: IMG_5081,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8462_photo: PhotoInfo = {
+  src: IMG_8462,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8736_photo: PhotoInfo = {
+  src: IMG_8736,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8747_photo: PhotoInfo = {
+  src: IMG_8747,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8762_photo: PhotoInfo = {
+  src: IMG_8762,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8828_photo: PhotoInfo = {
+  src: IMG_8828,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8830_photo: PhotoInfo = {
+  src: IMG_8830,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8838_photo: PhotoInfo = {
+  src: IMG_8838,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8841_photo: PhotoInfo = {
+  src: IMG_8841,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8847_photo: PhotoInfo = {
+  src: IMG_8847,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8851_photo: PhotoInfo = {
+  src: IMG_8851,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8853_photo: PhotoInfo = {
+  src: IMG_8853,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8854_photo: PhotoInfo = {
+  src: IMG_8854,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8861_photo: PhotoInfo = {
+  src: IMG_8861,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8867_1_photo: PhotoInfo = {
+  src: IMG_8867_1,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8877_photo: PhotoInfo = {
+  src: IMG_8877,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8926_photo: PhotoInfo = {
+  src: IMG_8926,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8937_photo: PhotoInfo = {
+  src: IMG_8937,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_8957_photo: PhotoInfo = {
+  src: IMG_8957,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_9253_photo: PhotoInfo = {
+  src: IMG_9253,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_9255_photo: PhotoInfo = {
+  src: IMG_9255,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_9272_photo: PhotoInfo = {
+  src: IMG_9272,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gm_IMG_9275_photo: PhotoInfo = {
+  src: IMG_9275,
+  tags: [WeddingServices.GLORIA_MICAEL],
+};
+
+export const gloriaMicaelPhotos: PhotoInfo[] = [
+  gm_IMG_5073_photo,
+  gm_IMG_5081_photo,
+  gm_IMG_8462_photo,
+  gm_IMG_8736_photo,
+  gm_IMG_8747_photo,
+  gm_IMG_8762_photo,
+  gm_IMG_8828_photo,
+  gm_IMG_8830_photo,
+  gm_IMG_8838_photo,
+  gm_IMG_8841_photo,
+  gm_IMG_8847_photo,
+  gm_IMG_8851_photo,
+  gm_IMG_8853_photo,
+  gm_IMG_8854_photo,
+  gm_IMG_8861_photo,
+  gm_IMG_8867_1_photo,
+  gm_IMG_8877_photo,
+  gm_IMG_8926_photo,
+  gm_IMG_8937_photo,
+  gm_IMG_8957_photo,
+  gm_IMG_9253_photo,
+  gm_IMG_9255_photo,
+  gm_IMG_9272_photo,
+  gm_IMG_9275_photo,
+];
