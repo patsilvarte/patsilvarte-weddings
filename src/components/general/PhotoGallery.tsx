315dafd89fe5a1fee121204c7ee25e8b798cc8cc
Patrícia Silva <psilva@imaginarycloud.com>
2026-02-12 18:06:43 +0000

    adjusts on photo gallery

--- src/components/general/PhotoGallery.tsx
+++ src/components/general/PhotoGallery.tsx
@@ -1,6 +1,8 @@
 import Masonry from "@mui/lab/Masonry";
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React, { useEffect, useState, type FC } from "react";
+import { Keyboard, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import type { PhotoInfo } from "../../info/types";
 import "./PhotoGallery.scss";
 import { PhotoGalleryDialog } from "./PhotoGalleryDialog";
@@ -10,6 +12,8 @@ interface PhotoGalleryProps {
 }
 
 export const PhotoGallery: FC<PhotoGalleryProps> = ({ photosList }) => {
+  const isMobile = !useMediaQuery("(min-width:900px)");
+
   const [loaded, setLoaded] = useState<Record<number, boolean>>({});
   const [open, setOpen] = React.useState(false);
   const [activeIndex, setActiveIndex] = React.useState<number>(0);
@@ -38,25 +42,45 @@ export const PhotoGallery: FC<PhotoGalleryProps> = ({ photosList }) => {
 
   return (
     <Box className="photo-gallery">
-      <Masonry columns={3} spacing={2}>
-        {photosList.map((item, index) => (
-          <div
-            key={index}
-            className={`photo-gallery__box photo-gallery__box--${
-              loaded[index] ? "visible" : "invisible"
-            }`}
-            onClick={() => handleOpen(index)}
-            style={{ cursor: "pointer" }}
-          >
-            <img
-              src={item.src}
-              loading="lazy"
-              onLoad={() => setLoaded((prev) => ({ ...prev, [index]: true }))}
-              className="photo-gallery__image"
-            />
-          </div>
-        ))}
-      </Masonry>
+      {isMobile ? (
+        <Swiper
+          modules={[Navigation, Keyboard, Pagination]}
+          initialSlide={activeIndex}
+          onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+          autoHeight={true}
+          grabCursor={true}
+          keyboard
+          loop
+          className="photo-gallery__swiper"
+          pagination={{ clickable: true }}
+        >
+          {photosList.map((photo, index) => (
+            <SwiperSlide key={index}>
+              <img src={photo.src} className="photo-gallery__image" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <Masonry columns={3} spacing={2}>
+          {photosList.map((item, index) => (
+            <div
+              key={index}
+              className={`photo-gallery__box photo-gallery__box--${
+                loaded[index] ? "visible" : "invisible"
+              }`}
+              onClick={() => handleOpen(index)}
+              style={{ cursor: "pointer" }}
+            >
+              <img
+                src={item.src}
+                loading="lazy"
+                onLoad={() => setLoaded((prev) => ({ ...prev, [index]: true }))}
+                className="photo-gallery__image"
+              />
+            </div>
+          ))}
+        </Masonry>
+      )}
       <PhotoGalleryDialog
         open={open}
         handleClose={handleClose}
