2e8efe2acb22363732a8fd98f14dcb327223144c
Patrícia Silva <psilva@imaginarycloud.com>
2026-02-13 17:51:04 +0000

    add initial config for images otimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), imagetools()],
   base: "/patsilvarte-weddings/", // repo name
 });
